5b7a79cf1eb2893deace98ec5f05f6b80f2bc76b
Joseph Pulaski <jpulaski79@gmail.com>
2016-12-02 14:30:53 -0500

    initial commit

---
+++ app/routes.js
@@ -0,0 +1,59 @@
+var User = require('./models/user');
+module.exports = function(app, passport){
+    app.get('/', function(req, res){
+        res.render('index.ejs');
+    });
+    //renders login.ejs and shows error messages
+    app.get('/login', function(req, res){
+        res.render('login.ejs', { message: req.flash('loginMessage') });
+    });
+    //post route for passport local-login strategy (passport.js)
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
+
+    app.get('/signup', function(req, res){
+        res.render('signup.ejs', { message: req.flash('signupMessage') });
+    });
+
+    //post route for passport local-login strategy (passport.js)
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/',
+        failureRedirect: '/signup',
+        failureFlash: true
+    }));
+    //profile page for auth'd users
+    app.get('/profile', isLoggedIn, function(req, res){
+        res.render('profile.ejs', { user: req.user });  //renders using info from user
+    });
+
+    app.get('/historic', isLoggedIn, function(req, res){
+        res.render('historic.ejs', { user: req.user });  //renders using info from user
+    });
+
+    //facebook auth route
+    app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
+    //facebook auth callback route
+    app.get('/auth/facebook/callback',
+        passport.authenticate('facebook', { successRedirect: '/profile',
+            failureRedirect: '/' }));
+
+
+    //logout route
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    })
+};
+
+//middleware function to check for auth logic
+//duplicate logic in future for different auth use cases
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    //need to build a page to send them to an account generation page
+    res.redirect('/login');
+}
---
+++ config/auth.js
@@ -0,0 +1,13 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID' : "203058520102046",
+        'clientSecret' : '799c4a6c83d2a11c57bf2aa63e144877',
+        'callbackURL': "http://localhost:3000/auth/facebook/callback"
+    },
+
+    'googleAuth' : {
+        'clientID': 'enter client id here',
+        'clientSecret': 'enter client secret here',
+        'callbackURL': 'enter callback here'
+    }
+}
